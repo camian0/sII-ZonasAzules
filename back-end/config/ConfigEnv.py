602a40af3ba572b4d62733ee6e2594357343c778
camian0 <camian6@outlook.com>
2024-05-29 20:26:03 -0500

    Load environment variables were configurated

--- back-end/config/ConfigEnv.py
+++ back-end/config/ConfigEnv.py
@@ -3,4 +3,9 @@ import os
 
 load_dotenv()
 
+ENGINE = os.getenv("engine")
+USER = os.getenv("user")
+PASSWORD = os.getenv("pass")
+HOST = os.getenv("host")
+DBNAME = os.getenv("db_name")
 SECRET = os.getenv("secret")
