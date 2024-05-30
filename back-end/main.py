41f38de2bd38e678d4c3a84481615fddbdc10f22
camian0 <camian6@outlook.com>
2024-05-29 22:30:39 -0500

    Uncessary route code was deleted

--- back-end/main.py
+++ back-end/main.py
@@ -1,7 +1,10 @@
 from typing import Union
 from fastapi import FastAPI
+from config.DB import Base, engine
 
 from routes.AuthRoute import authRouter
+
+# Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 app.include_router(authRouter)
@@ -10,8 +13,3 @@ app.include_router(authRouter)
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
