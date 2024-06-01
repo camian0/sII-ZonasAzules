19958a55058d658dc376bf41fc261833a3f82eda
camian0 <camian6@outlook.com>
2024-05-31 20:58:14 -0500

    Method to excecute model tables on order was added

--- back-end/main.py
+++ back-end/main.py
@@ -3,8 +3,19 @@ from fastapi import FastAPI
 from config.DB import Base, engine
 
 from routes.AuthRoute import authRouter
+from models.AuthUser import AuthUser
+from models.User import User
+from models.Role import Role
 
-# Base.metadata.create_all(bind=engine)
+
+def create_dbs():
+    Role.__table__.create(bind=engine, checkfirst=True)
+    AuthUser.__table__.create(bind=engine, checkfirst=True)
+    User.__table__.create(bind=engine, checkfirst=True)
+
+
+Base.metadata.create_all(bind=engine)
+create_dbs()
 
 app = FastAPI()
 app.include_router(authRouter)
