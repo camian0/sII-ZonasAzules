1b7e4577fe0e36870eec53983de7f29ceb5610c7
camian0 <camian6@outlook.com>
2024-05-31 20:56:49 -0500

    Changed email field default

--- back-end/schemas/UserAuthSchema.py
+++ back-end/schemas/UserAuthSchema.py
@@ -3,5 +3,7 @@ from pydantic import BaseModel, Field
 
 class UserAuthSchema(BaseModel):
 
-    email: str = Field(pattern="^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$")
+    email: str = Field(
+        pattern="^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$", default="email@server.com"
+    )
     password: str
