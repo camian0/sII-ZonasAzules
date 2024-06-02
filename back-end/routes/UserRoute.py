ce89ad03792e1327c5d90e5699bfb845e5d69739
camian0 <camian6@outlook.com>
2024-06-02 10:21:14 -0500

    Routes for users were configurated
    the routes for users were configurated, the route to get all users was
    adde

---
+++ back-end/routes/UserRoute.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+
+from config.DB import getDb
+from models.User import User
+from services.UserService import getUsers
+from services.AuthService import AuthService
+
+userRoutes = APIRouter(
+    prefix="/users", tags=["users"], dependencies=[Depends(AuthService())]
+)
+
+
+@userRoutes.get("/")
+def getAll(db: Session = Depends(getDb)):
+    query = getUsers(db)
+    if query:
+        return JSONResponse(content={"data": query}, status_code=200)
+
+    return JSONResponse(content={"No hay usuarios para mostrar."}, status_code=401)
