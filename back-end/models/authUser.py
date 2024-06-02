262d54917a4f42f40deb0d4d413172583c046225
camian0 <camian6@outlook.com>
2024-06-02 12:45:11 -0500

    fields changed

--- back-end/models/authUser.py
+++ back-end/models/authUser.py
@@ -1,21 +1,19 @@
 from config.dB import Base
-from sqlalchemy import Column, Integer, VARCHAR, ForeignKeyConstraint
+from sqlalchemy import Column, Integer, ForeignKey, VARCHAR
 from sqlalchemy.orm import relationship
 
 
 class AuthUser(Base):
     __tablename__ = "auth_users"
 
-    id = Column(Integer, primary_key=True)
-    email = Column(VARCHAR(255), unique=True, nullable=False)
-    password = Column(VARCHAR(255), nullable=False)
-    role_id = Column(Integer, nullable=False)
-    user_id = Column(Integer, nullable=False)
-
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(VARCHAR(50), unique=True)
+    password = Column(VARCHAR(100))
+    role_id = Column(Integer, ForeignKey("roles.id"), nullable=False)
     role = relationship("Role", back_populates="auth_users")
-    user = relationship("User", back_populates="auth_users")
+    authUser = relationship("User", back_populates="auth_user")
 
-    __table_args__ = (
-        ForeignKeyConstraint(["role_id"], ["roles.id"], name="fk_auth_user_role_id"),
-        ForeignKeyConstraint(["user_id"], ["users.id"], name="fk_auth_user_user_id"),
-    )
+    def dict(self):
+        return {
+            "email": self.email,
+        }
