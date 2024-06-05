84bfc9b4eac81f62aca40a3708eac67dba829454
camian0 <camian6@outlook.com>
2024-06-04 22:06:18 -0500

    Se agrego archivo de rutas de zonas azules
    Archivo para manejar las rutas de las zonas azules fue agregado, tambien
    una ruta para consultar una lista de zonas azules

---
+++ back-end/routes/blueZoneRoute.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import JSONResponse
+from sqlalchemy.orm import Session
+from sqlalchemy.exc import SQLAlchemyError
+
+from config.dB import getDb
+import traceback
+from services.AuthService import AuthService
+from helpers.logger import LOGGER
+from helpers.errorMessages import ERRORMESSAGE500, ERRORMESSAGE500DB
+from services.blueZoneService import get
+from schemas.pageSchema import PageSchema
+
+
+blueZoneRoute = APIRouter(
+    prefix="/blue-zone", tags=["Blue Zone"], dependencies=[Depends(AuthService())]
+)
+
+
+@blueZoneRoute.get("/")
+def getBlueZones(page: PageSchema, db: Session = Depends(getDb)):
+    try:
+        query = get(db, page=page.page, sizePage=page.sizePage)
+        if query:
+            return JSONResponse(content={"data": query}, status_code=200)
+
+        return JSONResponse(
+            content={"message": "No hay zonas azules para mostrar."}, status_code=200
+        )
+    except SQLAlchemyError as e:
+        traceBack = traceback.format_exc()
+        LOGGER.warning(f"error:{e}\n\n Traceback: {traceBack}")
+        return JSONResponse(
+            content=ERRORMESSAGE500DB,
+            status_code=500,
+        )
+    except Exception as e:
+        traceBack = traceback.format_exc()
+        LOGGER.error(f"error:{e}\n\n Traceback: {traceBack}")
+        return JSONResponse(
+            content=ERRORMESSAGE500,
+            status_code=500,
+        )
