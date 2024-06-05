28685af0133a5ea9e59d18438fa270a6df51a49c
camian0 <camian6@outlook.com>
2024-06-04 22:07:48 -0500

    Rutas para zonas azules fueron agregadas

--- back-end/main.py
+++ back-end/main.py
@@ -3,11 +3,13 @@ from fastapi import FastAPI
 
 from routes.AuthRoute import authRoute
 from routes.UserRoute import userRoutes
+from routes.blueZoneRoute import blueZoneRoute
 
 
 app = FastAPI()
 app.include_router(authRoute)
 app.include_router(userRoutes)
+app.include_router(blueZoneRoute)
 
 
 @app.get("/")
