7044b95ab5cb826b4479f270b0e780884c0aa17c
camian0 <camian6@outlook.com>
2024-06-06 00:10:45 -0500

    nombre de campo cambiado a calificación

--- back-end/schemas/ratingSchema.py
+++ back-end/schemas/ratingSchema.py
@@ -4,14 +4,15 @@ from pydantic import field_validator
 
 class RatingSchema(BaseModel):
     observation: str
-    rating: int
+    qualification: int
+    blue_zone_id: int
 
-    @field_validator("rating")
-    def ratingValidation(cls, rating):
+    @field_validator("qualification")
+    def qualificationValidation(cls, qualification):
 
-        if len(rating) < 0:
+        if qualification < 0:
             raise ValueError("La calificacion mínima es de 0 puntos.")
-        if len(rating) > 5:
+        if qualification > 5:
             raise ValueError("La calificación maxima es de 5 puntos.")
 
-        return rating
+        return qualification
