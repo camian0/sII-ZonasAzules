c8fb86959d9077a85a002b2e89a021e7d8905882
Eider22 <eidercardona22@gmail.com>
2024-06-05 21:55:08 -0500

    Se crea funcionalidad para crear y listar tipos de plazas. Se corrige uso de mensajes en servicio y rutas de áreas

--- back-end/helpers/responseMessages.py
+++ back-end/helpers/responseMessages.py
@@ -1,3 +1,12 @@
 ERRORMESSAGE500 = "Error interno en el servidor, no se pudo procesar la solicitud"
 ERRORMESSAGE500DB = "Error interno en el servidor, comuniquese con soporte."
-AREA_ALREARY_EXIST = "Ya existe la subárea"
+
+# Areas
+GET_ALL_AREAS_TYPE_OK = "Áreas obtenidas con éxito"
+AREA_ALREARY_EXIST = "Ya existe el área"
+CREATED_AREA_OK = "Área creada con éxito"
+
+# Place types
+GET_ALL_PLACE_TYPE_OK = "Tipos de plazas obtenidas con éxito"
+PLACE_TYPE_ALREARY_EXIST = "Ya existe el tipo de plaza"
+CREATED_PLACE_TYPE_OK = "Tipo de plaza creada con éxito"
