297652fbbddaf6ce5bc6ba2072c5e62037f736cf
camian0 <camian6@outlook.com>
2024-06-06 00:56:10 -0500

    Se agregó la páginación al metodo get

--- back-end/services/areaService.py
+++ back-end/services/areaService.py
@@ -4,13 +4,16 @@ from models.area import Area
 from schemas.areaSchema import AreaSchema
 from helpers.statusCodes import BAD_REQUEST, OK
 from helpers.dtos.responseDto import ResponseDto
+from helpers.helpers import queryPaginate
 
 
-def getAllAreas(db: Session) -> ResponseDto:
+def getAllAreas(page, sizePage, db: Session) -> ResponseDto:
     """
     Método para obtener todas las areas(sectores)
 
     Args:
+        page (_type_): numero de página
+        sizePage (_type_): registros por página
         db (Session): sesion de la base de datos
 
     Returns:
@@ -18,9 +21,10 @@ def getAllAreas(db: Session) -> ResponseDto:
     """
     responseDto = ResponseDto()
 
-    query = db.query(Area).all()
+    query = db.query(Area)
+    res = queryPaginate(query, page, sizePage)
 
-    areas = [i.dict() for i in query]
+    areas = [i.dict() for i in res]
     responseDto.status = OK
     responseDto.message = "Áreas obtenidas con éxito"
     responseDto.data = areas
