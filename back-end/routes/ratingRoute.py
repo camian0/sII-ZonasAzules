d5d06c50f131dd3d1c60599d43d63288be7ba854
camian0 <camian6@outlook.com>
2024-06-06 00:59:02 -0500

    funcion para obtener las valoraciones agregadó

--- back-end/routes/ratingRoute.py
+++ back-end/routes/ratingRoute.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Query
 from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 from sqlalchemy.exc import SQLAlchemyError
@@ -11,7 +11,7 @@ from helpers.responseMessages import ERRORMESSAGE500, ERRORMESSAGE500DB
 from helpers.statusCodes import BAD_REQUEST, OK, INTERNAL_SERVER_ERROR
 from schemas.ratingSchema import RatingSchema
 from helpers.dtos.responseDto import ResponseDto
-from services.ratingService import create
+from services.ratingService import create, getAll
 
 
 ratingRoute = APIRouter(
@@ -42,6 +42,43 @@ def createRate(rating: RatingSchema, db: Session = Depends(getDb)):
         )
     except Exception as e:
         db.rollback()
+        traceBack = traceback.format_exc()
+        LOGGER.error(f"error:{e}\n\n Traceback: {traceBack}")
+
+        responseDto = ResponseDto()
+        responseDto.status = INTERNAL_SERVER_ERROR
+        responseDto.message = ERRORMESSAGE500
+        return JSONResponse(
+            content=responseDto.toString(),
+            status_code=INTERNAL_SERVER_ERROR,
+        )
+
+
+@ratingRoute.get("/")
+def getRates(
+    page: int = Query(default=1),
+    sizePage: int = Query(default=10),
+    db: Session = Depends(getDb),
+):
+    response = ResponseDto()
+    try:
+        response = getAll(page, sizePage, db)
+        if response.status == OK:
+            return JSONResponse(content=response.toString(), status_code=OK)
+
+        return JSONResponse(content=response.toString(), status_code=BAD_REQUEST)
+    except SQLAlchemyError as e:
+        traceBack = traceback.format_exc()
+        LOGGER.warning(f"error:{e}\n\n Traceback: {traceBack}")
+
+        responseDto = ResponseDto()
+        responseDto.status = INTERNAL_SERVER_ERROR
+        responseDto.message = ERRORMESSAGE500DB
+        return JSONResponse(
+            content=responseDto.toString(),
+            status_code=INTERNAL_SERVER_ERROR,
+        )
+    except Exception as e:
         traceBack = traceback.format_exc()
         LOGGER.error(f"error:{e}\n\n Traceback: {traceBack}")
 
--- back-end/services/ratingService.py
+++ back-end/services/ratingService.py
@@ -4,6 +4,7 @@ from models.rating import Rating
 from schemas.ratingSchema import RatingSchema
 from helpers.statusCodes import OK
 from helpers.dtos.responseDto import ResponseDto
+from helpers.helpers import queryPaginate
 
 
 def create(rating: RatingSchema, db: Session) -> ResponseDto:
@@ -28,3 +29,27 @@ def create(rating: RatingSchema, db: Session) -> ResponseDto:
     response.message = "Puntuacion agregada con éxito"
     response.data = newRating.dict()
     return response
+
+
+def getAll(page, sizePage, db: Session) -> ResponseDto:
+    """
+        Metodo para Obtener todas las valoraciones
+
+    Args:
+        page (_type_): número de página
+        sizePage (_type_): Registros por página
+        db (Session):  sesión de la base de datos
+
+    Returns:
+        ResponseDto: devuelve un objeto response dto con los resultados de la transacción
+    """
+    responseDto = ResponseDto()
+
+    query = db.query(Rating)
+    res = queryPaginate(query, page, sizePage)
+
+    areas = [i.dict() for i in res]
+    responseDto.status = OK
+    responseDto.message = "Áreas obtenidas con éxito"
+    responseDto.data = areas
+    return responseDto
