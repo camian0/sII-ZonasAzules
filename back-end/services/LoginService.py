9d3bdc6f94123d12fb593c95330efe8243f2cab3
camian0 <camian6@outlook.com>
2024-06-07 22:08:14 -0500

    login cambiado para no encriptar contraseña
    al momento de hacer login, se quitó el atributo contraseña para generar
    el jwt de autenticación

--- back-end/services/LoginService.py
+++ back-end/services/LoginService.py
@@ -18,12 +18,12 @@ def login(userAuth: UserAuthSchema, db: Session) -> str | None:
     Returns:
         str | None: devuelve un token jwd como autenticacion exitosa
     """
-    print("antes de la queryu")
     query = db.query(AuthUser).filter(userAuth.email == AuthUser.email).first()
-    print("la query" , query)
     if query:
         if verifyPassword(userAuth.password, query.password):
+            delattr(userAuth, "password")
             userAuth.role_id = query.role_id
+
             token = encodeJwt(userAuth.__dict__)
             return token
     return None
