f7b2a1608bbab1e04ee6f241b44d6982cad387d9
camian0 <camian6@outlook.com>
2024-06-11 22:08:50 -0500

    modulos de la aplicacion agregados
    los modulos admin, auth, cards, restore fueron creados, cada módulo
    tiene su archivo de rutas internas, y administrarlas mejor, en el módulo
    auth se agregaron los componentes login, register y auth

---
+++ front-end/src/app/auth/auth.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { RestoreComponent } from './restore/restore.component';
+
+export const AUTH_ROUTES: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'restore', component: RestoreComponent },
+];
---
+++ front-end/src/app/auth/restore/restore.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-restore',
+  standalone: true,
+  templateUrl: './restore.component.html',
+  styleUrls: ['./restore.component.css'],
+})
+export class RestoreComponent {}
