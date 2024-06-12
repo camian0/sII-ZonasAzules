3a41c1007875474b1ebd000a34555600ab25c7c2
camian0 <camian6@outlook.com>
2024-06-11 22:05:59 -0500

    archivo de rutas principales terminado

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./auth/auth.routes').then((m) => m.AUTH_ROUTES),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.routes').then((m) => m.ADMIN_ROUTES),
+  },
+  {
+    path: 'cards',
+    loadChildren: () =>
+      import('./cards/cards.routes').then((m) => m.CARDS_ROUTES),
+  },
+  {
+    path: 'search',
+    loadChildren: () =>
+      import('./search/search.routes').then((m) => m.SEARCH_ROUTES),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
