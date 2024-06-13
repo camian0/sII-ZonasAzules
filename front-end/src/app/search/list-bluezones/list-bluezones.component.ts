dc7e1942ceee12b52b46ae3690778e285db233c4
camian0 <camian6@outlook.com>
2024-06-13 15:00:19 -0500

    terminada la lógica para listar zonas azules
    el codigo para listar zonas azules haciendo la petición al backend fue
    finalizada

--- front-end/src/app/search/list-bluezones/list-bluezones.component.ts
+++ front-end/src/app/search/list-bluezones/list-bluezones.component.ts
@@ -1,15 +1,25 @@
 import { Component } from '@angular/core';
 import { getData } from '../../../request/request';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { NotificationService } from '../../shared/services/notifications.service';
+import { MatTableModule } from '@angular/material/table';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-list-bluezones',
   standalone: true,
   templateUrl: './list-bluezones.component.html',
   styleUrls: ['./list-bluezones.component.css'],
+  imports: [MatTableModule, CommonModule],
 })
 export class ListBluezonesComponent {
+  token: string =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InVzZXJAZXhhbXBsZS5jb20iLCJyb2xlX2lkIjoyfQ.D8IqHKmqU1Sx3KgtPKvhhG-Tr9YsPnh8_KYPyCk7lC0';
   model: object;
-  constructor() {
+  blueZones = [];
+  displayedColumns: string[] = ['id', 'name', 'address', 'observation'];
+  dataSource = this.blueZones;
+  constructor(public notificationService: NotificationService) {
     this.model = {
       id: 0,
       name: '',
@@ -17,11 +27,21 @@ export class ListBluezonesComponent {
       observation: '',
     };
   }
+  ngOnInit() {
+    this.getBlueZones();
+  }
 
-  async getBlueZones() {
+  async getBlueZones(): Promise<void> {
+    localStorage.setItem('token', this.token);
     await getData('blue-zone', {})
       .then((res) => {
-        console.log('otra cosa');
+        if (res.status === 200) {
+          this.notificationService.success('Zona azules obtenidas con exito');
+          this.blueZones = res.data;
+          console.log('blue', this.blueZones);
+          return;
+        }
+        console.log('error', res.message);
       })
       .catch(() => {
         console.log('alguna cosa');
