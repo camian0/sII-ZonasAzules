83fee1fdd0107654263ac62ce055a10313e17521
camian0 <camian6@outlook.com>
2024-06-14 04:22:37 -0500

    el side nav fue agregado
    side nav fue agregado al componente principal, junto con el router
    outler para que alli se carguen todos los otros componentes

--- front-end/src/app/app.component.ts
+++ front-end/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'front-end-angular';
+  router = inject(Router);
+  token = localStorage.getItem('token');
+
+  logOut(): void {
+    localStorage.clear();
+    this.router.navigateByUrl('');
+    console.log('saliendo');
+  }
 }
